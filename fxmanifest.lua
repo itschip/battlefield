40f63ffcbc343a405e6623234dec429490b00435
Christopher <onlygamernerd@gmail.com>
2021-06-17 19:57:34 +0200

    update fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version "cerulean"
+game "gta5"
+
+author "chip"
+description "The source code for the Battlefield gamemode in FiveM"
+
+client_script "dist/client.js"
+server_script "dist/server.js"
