ff9410121cad76e12f1e689c70d172e3b145cbd5
Christopher <onlygamernerd@gmail.com>
2021-06-22 09:44:38 +0200

    deploy stuff

--- src/game/modules/core/client/deploy.ts
+++ src/game/modules/core/client/deploy.ts
@@ -1,5 +1,12 @@
-/* 
+/*
 I guess we need some sort of getting the coord for each zone,
 then we check what zone is selected, get the coord, as a obj property, and then spawn the player
 on that position.
 */
+
+// not even sure if this needs to be a event on the client side. We can just do it all server side.
+onNet('deployPlayer', () => {
+  const [x, y, z] = GetEntityCoords(PlayerPedId());
+
+  SetEntityCoords(PlayerPedId(), x, y, z, false, false, false, false);
+})
